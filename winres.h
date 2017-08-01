e8cb21ff014e54361960b85930612af6437112e7
P373RW4NG <peter.w4ng@gmail.com>
2017-08-01 23:48:10 +0800

    Add files via upload

---
+++ winres.h
@@ -0,0 +1,36 @@
+#ifndef WINRES_H
+#define WINRES_H
+
+#include <wtypes.h>
+#include <iostream>
+using namespace std;
+
+class winRes{
+public:
+    int height;
+    int width;
+    winRes(){
+        GetDesktopResolution(this);
+    }
+    winRes(int w, int h): width(w), height(h){}
+    ~winRes(){}
+    void printSize(){
+        cout<<"Screen resolusion: "<<width<<'*'<<height<<endl;
+    }
+
+private:
+    void GetDesktopResolution(winRes* screen)
+    {
+       RECT desktop;
+       // Get a handle to the desktop window
+       const HWND hDesktop = GetDesktopWindow();
+       // Get the size of screen to the variable desktop
+       GetWindowRect(hDesktop, &desktop);
+       // The top left corner will have coordinates (0,0)
+       // and the bottom right corner will have coordinates
+       screen->width = desktop.right;
+       screen->height = desktop.bottom;
+    }
+};
+
+#endif // WINRES_H
