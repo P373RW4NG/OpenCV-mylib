aecd21f8aee38c4aa4626f2009e284fdc7f1b458
P373RW4NG <peter.w4ng@gmail.com>
2017-11-06 23:05:44 +0800

    Update winres.h

--- winres.h
+++ winres.h
@@ -8,20 +8,38 @@ Platform: Windows
 #include <iostream>
 using namespace std;
 
+
 class winRes{
-public:
     int height;
     int width;
+public:
     winRes(){
+#if defined _WIN32
         GetDesktopResolution(this);
+#elif defined __APPLE__ || __MACH__
+        CGDirectDisplayID id = CGMainDisplayID();
+        width = CGDisplayPixelsHigh(id);
+        height = CGDisplayPixelsWide(id);
+#elif defined __linux__
+        Display* disp = XOpenDisplay(NULL);
+        //Screen* scrn = DefaultScreenOfDisplay(disp);
+        //width = scrn->width;
+        //height = scrn->height;
+        //Display *display = XGetMainDisplay(NULL);
+        const int scrn = DefaultScreen(disp);
+        width = DisplayWidth(disp, scrn);
+        height = DisplayHeight(disp, scrn);
+#endif
     }
     winRes(int w, int h): width(w), height(h){}
     ~winRes(){}
     void printSize(){
-        cout<<"Screen resolusion: "<<width<<'*'<<height<<endl;
+        std::cout<<"Screen resolution: "<<width<<'*'<<height<<std::endl;
     }
-
+    int const Height(){ return height; }
+    int const Width(){ return width; }
 private:
+    #if defined _WIN32
     void GetDesktopResolution(winRes* screen)
     {
        RECT desktop;
@@ -34,6 +52,16 @@ private:
        screen->width = desktop.right;
        screen->height = desktop.bottom;
     }
+#endif
+};
+
+class imgSize{
+public:
+    imgSize():height(0), width(0), channel(0){}
+    ~imgSize(){}
+    int height, width, channel;
+    int area(){ return height*width; }
+    void printSize(){ std::cout<<"Screen resolution: "<<width<<'*'<<height<<'*'<<channel<<std::endl; }
 };
 
 #endif // WINRES_H
